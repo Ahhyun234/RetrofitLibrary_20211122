4c2f87d40785cea3016e653664ae7f0bee139677
이아현 <suk03029@gamil.com>
2021-11-23 15:16:27 +0900

    페북 로그인 관련

--- app/src/main/java/com/nepplus/retrofitlibrary_20211122/LoginActivity.kt
+++ app/src/main/java/com/nepplus/retrofitlibrary_20211122/LoginActivity.kt
@@ -1,18 +1,27 @@
 package com.nepplus.retrofitlibrary_20211122
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.nepplus.retrofitlibrary_20211122.databinding.ActivityLoginBinding
 import com.nepplus.retrofitlibrary_20211122.datas.BasicResponse
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.security.MessageDigest
 
 lateinit var binding: ActivityLoginBinding
+lateinit var callbackManager = CallbackManager()
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,5 +82,44 @@ class LoginActivity : BaseActivity() {
 
     override fun setValues() {
 
+        callbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult?) {
+                Log.d("로그인","성공")
+            }
+
+            override fun onCancel() {
+
+            }
+
+            override fun onError(error: FacebookException?) {
+
+            }
+
+        })
+
+        getKeyHash()
+
+
+
+    }
+
+    fun getKeyHash(){
+        val info = packageManager.getPackageInfo(
+            "com.nepplus.retrofitlibrary_20211122",
+            PackageManager.GET_SIGNATURES
+        )
+        for (signature in info.signatures) {
+            val md: MessageDigest = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode,resultCode,data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
