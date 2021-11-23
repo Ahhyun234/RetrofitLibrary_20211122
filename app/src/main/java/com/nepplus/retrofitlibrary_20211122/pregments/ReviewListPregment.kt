11de7505a3219b7a6c77c51759d38dfa2d210d3b
이아현 <suk03029@gamil.com>
2021-11-23 17:15:45 +0900

    프레그먼트 에서의 데이터 바인딩

--- app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/ReviewListPregment.kt
+++ app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/ReviewListPregment.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.nepplus.retrofitlibrary_20211122.BaseActivity
 import com.nepplus.retrofitlibrary_20211122.R
+import com.nepplus.retrofitlibrary_20211122.binding
+import com.nepplus.retrofitlibrary_20211122.databinding.ReviewListPregmentListBinding
 
 class ReviewListPregment : Fragment() {
+
+    lateinit var binding: ReviewListPregmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.review_list_pregment_list,container,false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.review_list_pregment_list,container,false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
