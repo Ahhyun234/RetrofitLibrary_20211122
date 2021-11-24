32827da98889c880cbc2bf4ddd521fb0e68364ba
이아현 <suk03029@gamil.com>
2021-11-24 09:57:22 +0900

    뷰페이저 어댑터와 화면의 뷰 페이저 연결

--- app/src/main/java/com/nepplus/retrofitlibrary_20211122/MainActivity.kt
+++ app/src/main/java/com/nepplus/retrofitlibrary_20211122/MainActivity.kt
@@ -3,11 +3,14 @@ package com.nepplus.retrofitlibrary_20211122
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.nepplus.retrofitlibrary_20211122.adapters.MainViewPagerAdapter
 import com.nepplus.retrofitlibrary_20211122.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var mMainViewPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
@@ -21,6 +24,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mMainViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        binding.mainViewPager.adapter=mMainViewPagerAdapter
 
     }
 }
