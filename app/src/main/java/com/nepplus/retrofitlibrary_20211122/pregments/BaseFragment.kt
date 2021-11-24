2be7f3fe28e55618d66d3d7f690fb1ad1126bbbd
이아현 <suk03029@gamil.com>
2021-11-24 10:42:32 +0900

    프레그먼트 들도 API 호출할 수 있도록 함

--- app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/BaseFragment.kt
+++ app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/BaseFragment.kt
@@ -3,15 +3,22 @@ package com.nepplus.retrofitlibrary_20211122.pregments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.nepplus.retrofitlibrary_20211122.api.ServerAPIServiceInterface
+import com.nepplus.retrofitlibrary_20211122.api.ServerApi
 
 abstract class BaseFragment :Fragment() {
 
 
     lateinit var mContext: Context
 
+//    API 호출 명세를 담은 변수를 상속시켜줄것임
+    lateinit var apiService : ServerAPIServiceInterface
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         mContext = requireContext()
+        val retrofit = ServerApi.getRetrofit()
+        apiService = retrofit.create(ServerAPIServiceInterface::class.java)
     }
 
     abstract fun setupEvents()
--- app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/MyProfilePregment.kt
+++ app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/MyProfilePregment.kt
@@ -1,15 +1,12 @@
 package com.nepplus.retrofitlibrary_20211122.pregments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import com.nepplus.retrofitlibrary_20211122.R
 import com.nepplus.retrofitlibrary_20211122.databinding.MyprofileFragmentBinding
-import com.nepplus.retrofitlibrary_20211122.utils.ContextUtil
 
 class MyProfilePregment : BaseFragment() {
 
