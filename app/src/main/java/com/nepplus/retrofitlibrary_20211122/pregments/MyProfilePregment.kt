1dd7a6bbf4750bc3ae06e289cdbed0a03024cb9b
이아현 <suk03029@gamil.com>
2021-11-24 10:52:16 +0900

    내 프로필 프레그먼트 -> 내 정보 받아오기 실행  - GlideLibrary 추가함

--- app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/MyProfilePregment.kt
+++ app/src/main/java/com/nepplus/retrofitlibrary_20211122/pregments/MyProfilePregment.kt
@@ -5,8 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.nepplus.retrofitlibrary_20211122.R
 import com.nepplus.retrofitlibrary_20211122.databinding.MyprofileFragmentBinding
+import com.nepplus.retrofitlibrary_20211122.datas.BasicResponse
+import com.nepplus.retrofitlibrary_20211122.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfilePregment : BaseFragment() {
 
@@ -34,5 +40,27 @@ class MyProfilePregment : BaseFragment() {
 
     override fun setValues() {
 
+        getMyinfoFromServer()
+
+    }
+
+    fun getMyinfoFromServer(){
+        apiService.getRequestMyinfo(ContextUtil.getToken(mContext)).enqueue(object :Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+
+                    val br = response.body()!!
+
+                    binding.txtNickName.text = br.data.user.nickname
+                    Glide.with(mContext).load(br.data.user.profileImageUrl).into(binding.imgProfile)
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 }
